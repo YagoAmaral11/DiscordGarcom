6c08af47d7df0df5f39280c16761e7f8b948a1b1
doszp <yagoamaralsouza@gmail.com>
2025-02-07 19:45:56 -0300

    Dia 2 - Refatorações aqui e ali - Melhoras no sistema de configuração - Mudanças no sistema de FraseDoDia: ao inves de um timer para mandar mensagem, manda a mensagem depois de chegar no horário da frase - Melhoras no logging no console -- Agora o bot tem uma configuração padrão, iniciada sempre que uma configuração não for encontrada -- Métodos para carregar/escrever qualquer JSON

---
+++ BotCode/ConfigIO.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GarçomDoKitts.configs
+{
+    public static class ConfigIO
+    {
+        public static readonly string TokenPath = "token.json";
+        public static readonly string ConfigPath = "config.json";
+        public static readonly string DataPath = "data.json";
+
+        public static async Task LoadConfig()
+        {
+            using (StreamReader sr = new StreamReader(TokenPath))
+            {
+                string json = await sr.ReadToEndAsync();
+                TokenJSON token = JsonConvert.DeserializeObject<TokenJSON>(json);
+
+                Program.token = token;
+            }
+
+            using (StreamReader sr = new StreamReader(ConfigPath))
+            {
+                string json = await sr.ReadToEndAsync();
+                ConfigJSON config = JsonConvert.DeserializeObject<ConfigJSON>(json);
+
+                Program.config = config;
+            }
+        }
+
+        public static async Task WriteConfig()
+        {
+            string json = JsonConvert.SerializeObject(Program.config, Formatting.Indented);
+
+            using (StreamWriter sw = new StreamWriter(ConfigPath))
+            {
+                await sw.WriteAsync(json);
+            }
+        }
+
+        public static async Task Write(string location, object toWrite)
+        {
+            string json = JsonConvert.SerializeObject(toWrite, Formatting.Indented);
+
+            using (StreamWriter sw = new StreamWriter(location))
+            {
+                await sw.WriteAsync(json);
+            }
+        }
+
+        public static async Task<object> Load(string location, Type type)
+        {
+            string json;
+
+            using (StreamReader sr = new StreamReader(location))
+            {
+                json = await sr.ReadToEndAsync();
+                return JsonConvert.DeserializeObject(json, type);
+            }
+        }
+
+    }
+
+    public class TokenJSON
+    {
+        public string Token { get; set; }
+    }
+
+    public class ConfigJSON
+    {
+        // prefixo
+        public string Prefix { get; set; }
+
+        // logs
+        public bool Log_Ticks { get; set; } // Se os ticks principais do bot devem ser logados
+        public bool Log_LogTicks { get; set; } // Se os ticks de Logging (ticks secundários) devem ser logados
+
+        // Timer
+        public float Timers_TickTimerMs { get; set; } // O tempo, em milissegundos, que o loop principal do bot ocorrerá
+        public float Timers_LogTimerMs { get; set; } // O tempo, em milissegundos, que o loop de logging do bot ocorrerá
+        
+        // Frase Diária
+        public int FraseDiaria_HoraDeEnvio { get; set; } // O hora do dia que o bot enviará uma frase
+        public int FraseDiaria_MinsDeEnvio { get; set; } // Os minutos do dia que o bot enviará uma frase (é usado em conjunto com o HoraDeEnvio)
+        // Quantas frases existem no canal de frases; Usar a pesquisa do discord para descobrir; É usado quando o bot é iniciado pela primeiro vez;
+        // Esse valor é atualizado e guardado dentro do modulo de frase diária quando mensagens são excluidas e adicionadas no frases, mas somente quando o bot está online
+        // Logo, é importante que, no futuro, o bot guarde esse valor (junto com todas as instâncias de módulos) em um local separado, para que ele seja carregado.
+        public int FraseDiaria_total { get; set; } 
+        public ulong FraseDiaria_CanalFetchID { get; set; } // O ID do canal de frases (de onde elas devem ser puxadas)
+        public ulong FraseDiaria_CanalEnvioID { get; set; } // O ID do canal de frases para envio das frases
+
+
+        public ConfigJSON()
+        {
+            Prefix = "Garçom, ";
+
+            Log_Ticks = false;   
+            Log_LogTicks = true;
+
+            Timers_TickTimerMs = 100;
+            Timers_LogTimerMs = 1000;
+
+            FraseDiaria_HoraDeEnvio = 12;
+            FraseDiaria_MinsDeEnvio = 0;
+            FraseDiaria_total = 1168; // 07/02/2025 18:50
+            FraseDiaria_CanalFetchID = 935704934144434196;
+            FraseDiaria_CanalEnvioID = 832773492738490452;
+        }
+    }
+
+}
---
+++ BotCode/Program.cs
@@ -0,0 +1,317 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+using GarçomDoKitts.configs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace GarçomDoKitts
+{
+    internal class Program
+    {
+        public static TokenJSON token;
+        public static ConfigJSON config;
+
+        public static DiscordClient client;
+        public static CommandsNextExtension commands;
+
+        public static DiscordConfiguration discordConfiguration;
+        public static CommandsNextConfiguration commandsNextConfiguration;
+
+        public static Timer mainTimer;
+        public static Timer logTimer;
+
+        public static FraseDoDia dailyFrase = new FraseDoDia();
+
+        private static async Task Main(string[] args)
+        {
+            if (!File.Exists(ConfigIO.TokenPath))
+            {
+                Console.WriteLine($"(Program) {ConfigIO.TokenPath} não encontrado. Verificar se o arquivo existe e está com o nome correto");
+            }
+
+            if (!File.Exists(ConfigIO.ConfigPath))
+            {
+                Console.WriteLine($"(Program) {ConfigIO.ConfigPath} não encontrado. Verificar se o arquivo existe e está com o nome correto");
+                ConfigReset();
+            }
+
+            await ConfigIO.LoadConfig(); // Carrega configs
+            await InitDiscordConfig(); // Inicia o client do Discord para o bot            
+            await client.ConnectAsync(); // Conecta no Discord; Ao bot se conectar, a função de inicializar executa
+            await InitModules();
+            await Task.Delay(-1);
+        }
+
+        public async static void ConfigReset()
+        {
+            Console.WriteLine("(ConfigReset) Criando um novo arquivo de configuração em branco");
+            ConfigJSON json = new ConfigJSON();
+            await ConfigIO.Write($"{ConfigIO.ConfigPath}", json);
+        }
+
+        public async static void ConfigTemplate()
+        {
+            Console.WriteLine("(ConfigReset) Criando um novo arquivo de configuração de template");
+            ConfigJSON json = new ConfigJSON();
+            await ConfigIO.Write($"template-{ConfigIO.ConfigPath}", json);
+        }
+
+        private static Task InitDiscordConfig()
+        {
+            Console.WriteLine("(Program) Inicializando client e configurações do discord");
+
+            discordConfiguration = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = token.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            client = new DiscordClient(discordConfiguration);
+
+            commandsNextConfiguration = new CommandsNextConfiguration()
+            {
+                StringPrefixes = new string[] { config.Prefix },
+                EnableMentionPrefix = true,
+                EnableDefaultHelp = false,
+                EnableDms = true
+            };
+
+            commands = client.UseCommandsNext(commandsNextConfiguration);
+            client.Ready += Client_Ready;
+
+            Console.WriteLine("(Program) Inicialização finalizada");
+
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            Console.WriteLine("(DiscordClient) Client inicializado");            
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_MessageDeleted(DiscordClient sender, DSharpPlus.EventArgs.MessageDeleteEventArgs args)
+        {
+            Console.WriteLine("(DiscordClient) Evento Acionado: Mensagem deletada");
+            dailyFrase.FrasePossivelmenteDeletada(sender, args);
+
+            return Task.CompletedTask;
+        }
+
+        private static Task Client_MessageCreated(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs args)
+        {
+            Console.WriteLine("(DiscordClient) Evento Acionado: Mensagem criada");
+            dailyFrase.FrasePossivelmenteCriada(sender, args);
+
+            return Task.CompletedTask;
+        }
+
+        public static Task InitModules()
+        {
+            Console.WriteLine("(Program) Inicializando módulos");
+
+            // Módulos
+            Console.WriteLine("(Program) Inicializando Frase do Dia");
+            dailyFrase.Init();
+
+            // Eventos
+            Console.WriteLine("(Program) Inicializando Eventos");
+            client.MessageCreated += Client_MessageCreated;
+            client.MessageDeleted += Client_MessageDeleted;
+
+            // Timer Principal
+            Console.WriteLine("(Program) Inicializando timer principal");
+            mainTimer = new Timer(config.Timers_TickTimerMs);
+            mainTimer.AutoReset = true;
+            mainTimer.Enabled = true;
+            mainTimer.Elapsed += Loop;
+
+            // Timer Secundário
+            Console.WriteLine("(Program) Inicializando timer secundário");
+            logTimer = new Timer(config.Timers_LogTimerMs);
+            logTimer.AutoReset = true;
+            logTimer.Enabled = true;
+            logTimer.Elapsed += LogLoop;
+
+            return Task.CompletedTask;
+        }
+
+        private static void Loop(object sender, ElapsedEventArgs e)
+        {
+            if (config.Log_Ticks)
+            {
+                Console.WriteLine($"(Program) Bot Ticking in {DateTime.Now}");                
+            }
+
+            dailyFrase.Loop();
+        }
+
+        private static void LogLoop(object sender, ElapsedEventArgs e)
+        {
+            if (!config.Log_LogTicks)
+                return;
+
+            Console.WriteLine($"(Program) Bot log ticking in {DateTime.Now}");
+        }
+
+    }
+
+    public class FraseDoDia
+    {        
+        int fraseDoDiaEnvioHora;
+        int fraseDoDiaEnvioMins;
+        public int quantiaDeFrases; // quantas frases tem no canal de frases
+        
+        public DiscordChannel canalDeFrases; // origem das frases
+        public DiscordChannel canalParaReenviar; // destino das frases        
+
+        public Random random;
+        public DiscordMessage fraseDoDia; // qual a frase que foi escolhida para o dia.
+        bool fraseDoDiaEnviada = false;
+        DateTime diaUltimoEnvio;        
+
+        public void Init()
+        {
+            Console.WriteLine("(FraseDoDia) Inicializando");
+
+            fraseDoDiaEnviada = false;
+            fraseDoDiaEnvioHora = Program.config.FraseDiaria_HoraDeEnvio;
+            fraseDoDiaEnvioMins = Program.config.FraseDiaria_MinsDeEnvio;
+            
+            quantiaDeFrases = Program.config.FraseDiaria_total;
+
+            canalDeFrases = Program.client.GetChannelAsync(Program.config.FraseDiaria_CanalFetchID).Result;
+            canalParaReenviar = Program.client.GetChannelAsync(Program.config.FraseDiaria_CanalEnvioID).Result;
+
+            random = new Random();
+
+            Console.WriteLine("(FraseDoDia) Fim da inicialização");
+        }
+
+        public async Task Loop()
+        {            
+            if (!fraseDoDiaEnviada)
+            {
+                DateTime time = DateTime.Now;
+
+                if (time.Hour >= fraseDoDiaEnvioHora && time.Minute >= fraseDoDiaEnvioMins)
+                {
+                    // Deve enviar 
+
+                    fraseDoDiaEnviada = true;
+                    diaUltimoEnvio = time;
+
+                    await Daily();
+                }
+            }
+            else
+            {
+                // Verificar passagem do dia
+                if (DateTime.Now.Date.CompareTo(diaUltimoEnvio) > 0) 
+                {
+                    // É o próximo dia
+                    fraseDoDiaEnviada = false;   
+                }
+            }            
+        }
+
+        private async Task Daily()
+        {
+            Console.WriteLine("(FraseDoDia) Inicio de escolha de frase");
+            await Choose();
+            Console.WriteLine("(FraseDoDia) Fim da escolha de frase");
+
+            Console.WriteLine("(FraseDoDia) Inicio de envio");
+            await Send();
+            Console.WriteLine("(FraseDoDia) Fim de envio");
+        }
+
+        // Serve para enviar a frase do dia
+        public async Task Send()
+        {
+            Console.WriteLine("(FraseDoDia) Construindo mensagem para envio");
+
+            DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder();
+            embedBuilder.Title = "Frase do Dia";
+            embedBuilder.Color = new Optional<DiscordColor>(new DiscordColor("d619bd"));
+            embedBuilder.Footer = new DiscordEmbedBuilder.EmbedFooter();
+            embedBuilder.Footer.Text = $"Frase cunhada por {fraseDoDia.Author.Username}";
+            embedBuilder.Footer.IconUrl = fraseDoDia.Author.AvatarUrl;
+            embedBuilder.Timestamp = fraseDoDia.CreationTimestamp;
+            embedBuilder.Description = fraseDoDia.Content;            
+
+            DiscordEmbed embed = embedBuilder.Build();
+
+            Console.WriteLine("(FraseDoDia) Mensagem construída");
+            Console.WriteLine("(FraseDoDia) Iniciando envio da mensagem pelo client");
+
+            await canalParaReenviar.SendMessageAsync(embed);
+
+            Console.WriteLine("(FraseDoDia) Mensagem enviada pelo client");
+            Console.WriteLine($"(FraseDoDia) Enviada frase '{fraseDoDia.Content}' de {fraseDoDia.Author.Username}, criada em {fraseDoDia.CreationTimestamp}");
+        }
+
+        // Serve para escolher a frase do dia
+        public async Task Choose()
+        {
+            Console.WriteLine($"(FraseDoDia) Escolhendo um número aleatório para a frase do dia");
+
+            int indexDaFrase = random.Next(0, quantiaDeFrases);
+
+            Console.WriteLine($"(FraseDoDia) Procurando pela frase de índice {indexDaFrase}");
+
+            if (indexDaFrase < 100)
+            {
+                // Frase é uma das 100 primeiras
+                var frases = await canalDeFrases.GetMessagesAsync(100);
+                fraseDoDia = frases[indexDaFrase];
+            }
+            else
+            {                
+                // Frase é depois das 100 primeiras
+
+                IReadOnlyList<DiscordMessage> frases = null;
+                // pega a primeira mensagem para servir de base
+                var primeirasMsgs = await canalDeFrases.GetMessagesAsync(100); 
+                DiscordMessage msgPivot = primeirasMsgs[99];
+                int currentIndex = indexDaFrase; // usado para pegar a mensagens anteriores
+
+                // "Procura" a mensagem do índice passado, até puxar as mensagens dentro desse índice
+                while (currentIndex > 99)
+                {                    
+                    await Task.Delay(300);
+                    frases = await canalDeFrases.GetMessagesBeforeAsync(msgPivot.Id, 100);
+                    currentIndex -= 100;
+                    msgPivot = frases[99];                    
+                }
+
+                fraseDoDia = frases[currentIndex];
+            }
+
+            Console.WriteLine($"(FraseDoDia) Frase do dia escolhida: {fraseDoDia.Content} escrita por {fraseDoDia.Author.Username}");
+        }
+
+        public void FrasePossivelmenteDeletada(DiscordClient sender, DSharpPlus.EventArgs.MessageDeleteEventArgs args)
+        {
+            if (args.Channel == canalDeFrases)
+            {
+                quantiaDeFrases--;
+            }
+        }
+
+        public void FrasePossivelmenteCriada(DiscordClient sender, DSharpPlus.EventArgs.MessageCreateEventArgs args)
+        {
+            if (args.Channel == canalDeFrases)
+            {
+                quantiaDeFrases++;
+            }
+        }
+    }
+
+}
